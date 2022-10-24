8b2f7e0139ae72024e4fc43df660d623919a13d4
andym129 <andy_m129@163.com>
2022-10-24 21:30:17 +0800

    feature(ios16_paste_alert): iOS16 剪贴板弹窗 调研 - 剪贴板内容检测

--- Example/AMKCategories/AMKStackViewController.m
+++ Example/AMKCategories/AMKStackViewController.m
@@ -82,6 +82,38 @@
             pasteControl;
         })];
     }
+    
+    // 示例3
+    if (@available(iOS 14.0, *)) {
+        [self.stackView addArrangedSeparatorWithTitle:nil color:UIColor.clearColor size:60];
+        [self.stackView addArrangedSeparatorWithTitle:@"检测 UIPasteboard 内容" color:nil size:12];
+        [self.stackView addArrangedButton:@"bdwenku://wenku/operation?type=2&source=xxx&url=xxx" controlEvents:UIControlEventTouchUpInside block:^(UIButton *sender) {
+            UIPasteboard.generalPasteboard.string = sender.currentTitle;
+        }];
+        [self.stackView addArrangedButton:@"度口令：#bdwenku://wenku/operation?type=2&source=xxx&url=xxx#" controlEvents:UIControlEventTouchUpInside block:^(UIButton *sender) {
+            UIPasteboard.generalPasteboard.string = sender.currentTitle;
+        }];
+        [self.stackView addArrangedButton:@"eyJyZWZlciI6IjEwMjgyMDBvX3drd2ViNzYxNDEiLCJkb2NfaWQiOiJjNjg5ZDQ2M2ZiYjA2OWRjNTAyMmFhZWE5OThmY2MyMmJkZDE0M2QxIiwicm91dGVyIjoiYmR3ZW5rdTovL3dlbmt1L29wZXJhdGlvbj90eXBlPTEwOCZmcm9tPXdrc3Qmd2tzdF9yZWZlcj0xMDI4MjAwbyZkb2NfaWQ9YzY4OWQ0NjNmYmIwNjlkYzUwMjJhYWVhOTk4ZmNjMjJiZGQxNDNkMSZjYXJyeT0lN0IlMjJmcm9tJTIyJTNBJTIybWluaWFwcCUyMiUyQyUyMmRvY0lkJTIyJTNBJTIyYzY4OWQ0NjNmYmIwNjlkYzUwMjJhYWVhOTk4ZmNjMjJiZGQxNDNkMSUyMiUyQyUyMndvcmQlMjIlM0ElMjIlRTUlODUlQTUlRTUlODUlOUElRTclOTQlQjMlRTglQUYlQjclMjIlN0QiLCJmcm9tIjoid2Vua3VhcHAifQ==" controlEvents:UIControlEventTouchUpInside block:^(UIButton *sender) {
+            UIPasteboard.generalPasteboard.string = sender.currentTitle;
+        }];
+        [self.stackView addArrangedButton:@"bdwenku://?eyJyZWZlciI6IjEwMjgyMDBvX3drd2ViNzYxNDEiLCJkb2NfaWQiOiJjNjg5ZDQ2M2ZiYjA2OWRjNTAyMmFhZWE5OThmY2MyMmJkZDE0M2QxIiwicm91dGVyIjoiYmR3ZW5rdTovL3dlbmt1L29wZXJhdGlvbj90eXBlPTEwOCZmcm9tPXdrc3Qmd2tzdF9yZWZlcj0xMDI4MjAwbyZkb2NfaWQ9YzY4OWQ0NjNmYmIwNjlkYzUwMjJhYWVhOTk4ZmNjMjJiZGQxNDNkMSZjYXJyeT0lN0IlMjJmcm9tJTIyJTNBJTIybWluaWFwcCUyMiUyQyUyMmRvY0lkJTIyJTNBJTIyYzY4OWQ0NjNmYmIwNjlkYzUwMjJhYWVhOTk4ZmNjMjJiZGQxNDNkMSUyMiUyQyUyMndvcmQlMjIlM0ElMjIlRTUlODUlQTUlRTUlODUlOUElRTclOTQlQjMlRTglQUYlQjclMjIlN0QiLCJmcm9tIjoid2Vua3VhcHAifQ==" controlEvents:UIControlEventTouchUpInside block:^(UIButton *sender) {
+            UIPasteboard.generalPasteboard.string = sender.currentTitle;
+        }];
+        [self.stackView addArrangedButton:@"检测" controlEvents:UIControlEventTouchUpInside block:^(id sender) {
+            NSSet *patterns = [NSSet setWithArray:@[UIPasteboardDetectionPatternProbableWebURL, UIPasteboardDetectionPatternNumber]];
+            [UIPasteboard.generalPasteboard detectPatternsForPatterns:patterns completionHandler:^(NSSet<UIPasteboardDetectionPattern> *set, NSError *error) {
+                NSLog(@"%@, %@", set, error);
+                [weakSelf showAlertWithTitle:@"检测结果" content:error?:set];
+            }];
+        }];
+        [self.stackView addArrangedButton:@"检测" controlEvents:UIControlEventTouchUpInside block:^(id sender) {
+            NSSet *patterns = [NSSet setWithArray:@[UIPasteboardDetectionPatternProbableWebURL, UIPasteboardDetectionPatternNumber]];
+            [UIPasteboard.generalPasteboard detectValuesForPatterns:patterns completionHandler:^(NSDictionary<UIPasteboardDetectionPattern,id> *dict, NSError *error) {
+                NSLog(@"%@, %@", dict, error);
+                [weakSelf showAlertWithTitle:@"检测结果" content:error?:dict];
+            }];
+        }];
+    }
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -118,6 +150,15 @@
 #pragma mark - Layout Subviews
 
 #pragma mark - Action Methods
+
+- (void)showAlertWithTitle:(NSString *)title content:(id)content {
+    dispatch_async(dispatch_get_main_queue(), ^(void) {
+        NSString *message = [content description];
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+        [alertController addAction:[UIAlertAction actionWithTitle:@"好的" style:UIAlertActionStyleCancel handler:nil]];
+        [UIViewController.amk_topViewController presentViewController:alertController animated:YES completion:nil];
+    });
+}
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     [super touchesBegan:touches withEvent:event];
