180d5b9b1bfd28d7b0488e4c17a9de65300ae612
mengxinxin <mengxinxin@baidu.com>
2025-07-15 20:46:56 +0800

    feature(wk10090): 新建示例页

---
+++ Example/AMKCategories/Modules/Wenku/10090/AMK10090ExampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  AMK10090ExampleViewController.h
+//  AMKCategories_Example
+//
+//  Created by Meng Xinxin on 2025/7/15.
+//  Copyright © 2025 AndyM129. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMK10090ExampleViewController : UIViewController
+
+@end
---
+++ Example/AMKCategories/Modules/Wenku/10090/AMK10090ExampleViewController.m
@@ -0,0 +1,80 @@
+//
+//  AMK10090ExampleViewController.m
+//  AMKCategories_Example
+//
+//  Created by Meng Xinxin on 2025/7/15.
+//  Copyright © 2025 AndyM129. All rights reserved.
+//
+
+#import "AMK10090ExampleViewController.h"
+
+@interface AMK10090ExampleViewController ()
+
+@end
+
+@implementation AMK10090ExampleViewController
+
++ (void)load {
+    id __block token = [NSNotificationCenter.defaultCenter addObserverForName:UIApplicationDidFinishLaunchingNotification object:nil queue:NSOperationQueue.mainQueue usingBlock:^(NSNotification * _Nonnull notification) {
+        [NSNotificationCenter.defaultCenter removeObserver:token];
+        [UIViewController amk_pushViewController:[self new] animated:YES];
+    }];
+}
+
+#pragma mark - Dealloc
+
+- (void)dealloc {
+    
+}
+
+#pragma mark - Init Methods
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        self.title = NSStringFromClass(self.class);
+        self.hidesBottomBarWhenPushed = YES;
+    }
+    return self;
+}
+
+#pragma mark - Life Cycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = self.view.backgroundColor ?: [UIColor whiteColor];
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+}
+
+#pragma mark - Getters & Setters
+
+#pragma mark - Data & Networking
+
+#pragma mark - Layout Subviews
+
+#pragma mark - Action Methods
+
+#pragma mark - Notifications
+
+#pragma mark - KVO
+
+#pragma mark - Protocol
+
+#pragma mark - Helper Methods
+
+@end
