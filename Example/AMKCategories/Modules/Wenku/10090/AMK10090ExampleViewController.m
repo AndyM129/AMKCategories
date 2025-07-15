0b8b03e8176b63a093fb1698f88eb85b535426ca
mengxinxin <mengxinxin@baidu.com>
2025-07-15 20:56:00 +0800

    +1

--- Example/AMKCategories/Modules/Wenku/10090/AMK10090ExampleViewController.m
+++ Example/AMKCategories/Modules/Wenku/10090/AMK10090ExampleViewController.m
@@ -8,8 +8,8 @@
 
 #import "AMK10090ExampleViewController.h"
 
-@interface AMK10090ExampleViewController ()
-
+@interface AMK10090ExampleViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic, strong, readwrite, nullable) UITableView *tableView;
 @end
 
 @implementation AMK10090ExampleViewController
@@ -42,7 +42,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = self.view.backgroundColor ?: [UIColor whiteColor];
-    
+    [self.tableView reloadData];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -63,6 +63,36 @@
 
 #pragma mark - Getters & Setters
 
+- (UITableView *)tableView {
+    if (!_tableView) {
+        _tableView = [UITableView.alloc initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        _tableView.backgroundColor = [UIColor whiteColor];
+        _tableView.estimatedRowHeight = 0;
+        _tableView.estimatedSectionFooterHeight = 0;
+        _tableView.estimatedSectionHeaderHeight = 0;
+        //_tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        //_tableView.separatorColor = UIColor.clearColor;
+        _tableView.tableFooterView = [UIView.alloc initWithFrame:CGRectMake(0, 0, _tableView.frame.size.width, CGFLOAT_MIN)];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        if (@available(iOS 15.0, *)) {
+            _tableView.sectionHeaderTopPadding = 0;
+        }
+        if (@available(iOS 13.0, *)) {
+            _tableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+        } else {
+#           pragma clang diagnostic push
+#           pragma clang diagnostic ignored "-Wdeprecated-declarations"
+            self.automaticallyAdjustsScrollViewInsets = NO;
+#           pragma clang diagnostic pop
+        }
+        [_tableView registerClass:UITableViewCell.class forCellReuseIdentifier:NSStringFromClass(UITableViewCell.class)];
+        [self.view addSubview:_tableView];
+    }
+    return _tableView;
+}
+
 #pragma mark - Data & Networking
 
 #pragma mark - Layout Subviews
@@ -74,6 +104,44 @@
 #pragma mark - KVO
 
 #pragma mark - Protocol
+
+#pragma mark UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (section == 0) {
+        return 5;
+    }
+    if (section == 1) {
+        return 1;
+    }
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass(UITableViewCell.class) forIndexPath:indexPath];
+    cell.contentView.backgroundColor = [UIColor colorWithRed:71/255.0 green:158/255.0 blue:226/255.0 alpha:0.6 - indexPath.item * 0.05];
+    return cell;
+}
+
+#pragma mark UITableViewDelegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section == 0) {
+        return 100;
+    }
+    if (indexPath.section == 1) {
+        return 400;
+    }
+    return 0;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
 
 #pragma mark - Helper Methods
 
