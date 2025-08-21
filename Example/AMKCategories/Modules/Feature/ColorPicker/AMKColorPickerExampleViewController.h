fe291d7e6f8e4c207c1c07a09218be80ffdc0d79
mengxinxin <mengxinxin@baidu.com>
2025-08-21 10:16:04 +0800

    feat(color_picker): 新增示例页

---
+++ Example/AMKCategories/Modules/Feature/ColorPicker/AMKColorPickerExampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  AMKColorPickerExampleViewController.h
+//  AMKCategories_Example
+//
+//  Created by Meng Xinxin on 2025/8/21.
+//  Copyright © 2025 AndyM129. All rights reserved.
+//
+
+#import "AMKStackViewController.h"
+
+@interface AMKColorPickerExampleViewController : AMKStackViewController
+
+@end
---
+++ Example/AMKCategories/Modules/Feature/ColorPicker/AMKColorPickerExampleViewController.m
@@ -0,0 +1,66 @@
+//
+//  AMKColorPickerExampleViewController.m
+//  AMKCategories_Example
+//
+//  Created by Meng Xinxin on 2025/8/21.
+//  Copyright © 2025 AndyM129. All rights reserved.
+//
+
+#import "AMKColorPickerExampleViewController.h"
+
+@implementation AMKColorPickerExampleViewController
+
+#pragma mark - Dealloc
+
+- (void)dealloc {
+    
+}
+
+#pragma mark - Init Methods
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        self.title = NSStringFromClass(self.class);
+    }
+    return self;
+}
+
+#pragma mark - Life Circle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+}
+
+#pragma mark - Getters & Setters
+
+#pragma mark - Data & Networking
+
+#pragma mark - Layout Subviews
+
+#pragma mark - Action Methods
+
+#pragma mark - Notifications
+
+#pragma mark - KVO
+
+#pragma mark - Protocol
+
+#pragma mark - Helper Methods
+
+@end
