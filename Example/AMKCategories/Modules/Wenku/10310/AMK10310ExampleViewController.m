b7e507854ca0fc36f166710687ce790d11d1bb5b
mengxinxin <mengxinxin@baidu.com>
2026-04-21 20:38:14 +0800

    +1

--- Example/AMKCategories/Modules/Wenku/10310/AMK10310ExampleViewController.m
+++ Example/AMKCategories/Modules/Wenku/10310/AMK10310ExampleViewController.m
@@ -21,12 +21,12 @@
 
 @implementation AMK10310ExampleViewController
 
-+ (void)load {
-    id __block token = [NSNotificationCenter.defaultCenter addObserverForName:UIApplicationDidFinishLaunchingNotification object:nil queue:NSOperationQueue.mainQueue usingBlock:^(NSNotification * _Nonnull note) {
-        [NSNotificationCenter.defaultCenter removeObserver:token];
-        [UIViewController amk_pushViewController:[self.alloc init] animated:YES];
-    }];
-}
+//+ (void)load {
+//    id __block token = [NSNotificationCenter.defaultCenter addObserverForName:UIApplicationDidFinishLaunchingNotification object:nil queue:NSOperationQueue.mainQueue usingBlock:^(NSNotification * _Nonnull note) {
+//        [NSNotificationCenter.defaultCenter removeObserver:token];
+//        [UIViewController amk_pushViewController:[self.alloc init] animated:YES];
+//    }];
+//}
 
 #pragma mark - Dealloc
 
